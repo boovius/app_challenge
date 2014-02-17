dfd82232c6569313df038acd84836c9289df3e6f
Joshua Book <joshua.book@gmail.com>
2014-02-17 15:32:39 -0800

    integrate current apps into smart table on home

--- app/assets/javascripts/controllers/home_controller.js
+++ app/assets/javascripts/controllers/home_controller.js
@@ -1,6 +1,12 @@
 AppChallenge.controller('HomeController', ['$scope', 'currentApps', function($scope, currentApps) {
   $scope.stuff = "AHAHAHA"
   $scope.currentApps = currentApps
+  $scope. dataTableConfig
+
+  $scope.homeTableColumns = [
+        {label: 'Title', map: 'title'},
+        {label: 'Developers', map:'developer'}
+  ]
 
   console.log($scope)
   console.log(currentApps)
