f046c1caa6924db732dfc4435200b697224c5850
Joshua Book <joshua.book@gmail.com>
2014-02-17 16:12:49 -0800

    set before save filter to set done status on feature upon creation

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -8,4 +8,12 @@ class Feature < ActiveRecord::Base
     :app_id
 
   belongs_to :app
+
+  before_save :set_initial_done_state
+
+  private
+
+  def set_initial_done_state
+    self.done = false if self.done == nil
+  end
 end
