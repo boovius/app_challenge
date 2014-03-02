39ae395d3ec8fa82e3b61f4df0b768a495ab823f
Joshua Book <joshua.book@gmail.com>
2014-03-02 15:57:19 -0800

    add mechanism to create new features front end

--- app/assets/javascripts/controllers/app_create_controller.js
+++ app/assets/javascripts/controllers/app_create_controller.js
@@ -1,6 +1,24 @@
 AppChallenge.controller('AppCreateController', ['$scope', 'AppService', function($scope, AppService) {
 
-  $scope.app = {userId: gon.currentUser.id }
+  $scope.app = {
+    userId: gon.currentUser.id,
+    features: []
+  }
+
+  console.log(angular.isDefined($scope.app.features))
+  console.log($scope.app.features)
+
+  $scope.addNewFeature = function(){
+    var index = $scope.app.features.length > 0 ? $scope.app.features.length : 0
+    var new_feature =  {id: $scope.app.features[index]}
+    console.log(new_feature)
+    $scope.app.features.push(new_feature)
+    console.log($scope.app.features)
+  }
+
+  $scope.deleteFeature = function(index){
+    $scope.app.features.splice(index)
+  }
 
   $scope.createApp = function(){
     AppService.apps.save(angular.snakeize($scope.app))
