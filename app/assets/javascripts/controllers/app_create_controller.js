196c358d6ce6c211eb1027f3740d5fd589c8523d
Joshua Book <joshua.book@gmail.com>
2014-03-02 14:51:47 -0800

    implement angular.snakeize to keep consistent camel to JS and snake to ruby

--- app/assets/javascripts/controllers/app_create_controller.js
+++ app/assets/javascripts/controllers/app_create_controller.js
@@ -1,12 +1,8 @@
 AppChallenge.controller('AppCreateController', ['$scope', 'AppService', function($scope, AppService) {
 
-  console.log(gon.currentUser.id)
-
-  $scope.app = { user_id: gon.currentUser.id }
-  console.log($scope.app)
+  $scope.app = {userId: gon.currentUser.id }
 
   $scope.createApp = function(){
-    console.log($scope.app)
-    AppService.apps.save($scope.app)
+    AppService.apps.save(angular.snakeize($scope.app))
   }
 }])
--- app/assets/javascripts/initialize.js
+++ app/assets/javascripts/initialize.js
@@ -15,3 +15,16 @@ AppChallenge = angular.module('AppChallenge', [
 
 }])
 
+// Repurposed function to convert camelCase to snake_case
+// https://github.com/nathan7/snakeize
+angular.snakeize = function(obj) {
+  if (!obj || typeof obj !== 'object' || obj instanceof Date === true) return obj;
+  if (Array.isArray(obj)) return obj.map(angular.snakeize);
+  return Object.keys(obj).reduce(function (acc, key) {
+      var camel = key[0].toLowerCase() + key.slice(1).replace(/([A-Z]+)/g, function (m, x) {
+          return '_' + x.toLowerCase();
+      });
+      acc[camel] = angular.snakeize(obj[key]);
+      return acc;
+  }, {});
+}
