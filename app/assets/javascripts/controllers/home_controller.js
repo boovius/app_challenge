e1fb43fac605b9f60b30b76dc464ebc11d153a3b
Joshua Book <joshua.book@gmail.com>
2014-03-05 20:33:14 -0800

    fixes to master - check if user signed in on nav layout, revert app name on current sprint table

--- app/assets/javascripts/controllers/home_controller.js
+++ app/assets/javascripts/controllers/home_controller.js
@@ -6,7 +6,7 @@ AppChallenge.controller('HomeController', ['$scope', 'currentApps', function($sc
   }
 
   $scope.homeTableColumns = [
-        {label: 'App', map: 'title', cellTemplateUrl: ('assets/cell_templates/link.html')},
+        {label: 'App', map: 'title'},
         {label: 'Developer', map:'developer'},
         {label: 'Percent Completed', map:'percentageComplete', cellTemplateUrl: ('assets/cell_templates/percentage.html')}
   ]
